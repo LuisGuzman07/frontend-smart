02038272842f55ac25d83793ab608ba44a95b262
Luis David Guzman Rojas <guzmanluisdavid12@gmail.com>
2025-11-12 12:11:53 -0400

    Update base URL in axios configuration

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Configurar la URL base desde variables de entorno
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const apiClient = axios.create({
 	baseURL: API_BASE_URL,
